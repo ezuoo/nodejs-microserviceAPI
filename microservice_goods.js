852439bd9cb8c6ecd9448798bfa8a55a8e061a62
juho0428@naver.com <juho0428@naver.com>
2020-12-15 15:27:53 +0900

    checkout microservice goods

---
+++ microservice_goods.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const business = require("../nodejs-monolithicAPI/monolithic_goods");
+
+class goods extends require('./server') {
+    constructor() {
+        super("goods"
+            , process.argv[2] ? Number(process.argv[2]) : 9010
+            , ["POST/goods", "GET/goods", "DELETE/goods"]
+        );
+        this.connectToDistributor("127.0.0.1", 9000, (data) => {
+            console.log("Distributor Notification", data);
+        });
+    }
+
+    onRead(socket, data) {
+        console.log("onRead", socket.remoteAddress, socket.remotePort, data);
+        business.onRequest(socket, data.method, data.uri, data.params, (s, socket) => {
+            socket.write(JSON.stringify(packet)+ '¶');
+        });
+    }
+}
+
+new goods();
